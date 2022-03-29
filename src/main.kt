193a041a4c0a4b83871cecdf4d82d5f3f4ce5b8e
Nyakuar <nyakuaryy@gmail.com>
2022-03-29 10:05:39 +0300

    changes

--- src/main.kt
+++ src/main.kt
@@ -17,7 +17,7 @@ fun countries(){
  }
 }
 fun sub(){
- var numbers= arrayOf(32,17,4,213,78,43,90,31,3,73,11,158,62)
+ var numbers= arrayOf(32,17,4,213,45,45,45,90,31,3,73,11,158,62)
  var sum= numbers[1]+numbers[4]
  println(sum)
  println(numbers.indexOf(158))
